79acab7537e7cf6f0c04b010a9a7b8f3cf7925e9
Marco Quintella <2om.marco.quintella@gmail.com>
2019-05-24 19:15:20 -0300

    Initializing Files for FBtn

---
+++ btn.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class FBtn extends Vue {
+}
+</script>
---
+++ index.ts
@@ -0,0 +1,5 @@
+import FBtn from './btn.vue'
+
+export default Vue => {
+  Vue.component('FBtn', FBtn)
+}
